58917fded4650e1f180c2a0597564a008d217442
Matt Snell <msnell@unomaha.edu>
2022-05-03 08:34:42 -0500

    fix social API consistency issue

--- backend/social_api_testcase.py
+++ backend/social_api_testcase.py
@@ -1,9 +1,9 @@
 import requests
 
 def test_twitter():
-    random_song = requests.get("https://8380s22tm5app.com/random-song/")
+    random_song = requests.get("https://8380s22tm5app.com/twitter-post/")
     return random_song.status_code==200
 
 def test_facebook():
-    weather_song = requests.get("https://8380s22tm5app.com/weather-song/68123/")
+    weather_song = requests.get("https://8380s22tm5app.com/facebook-post/")
     return weather_song.status_code==200
